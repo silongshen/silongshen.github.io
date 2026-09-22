04a5a2ef6c50f80a5bbfb85fb5e31e5794045ff5
silongshen <www.jsak@qq.com>
2026-09-22 11:39:13 +0800

    docs: 内置在线文档文章图片

--- src/components/PreviewImage.tsx
+++ src/components/PreviewImage.tsx
@@ -1,15 +1,21 @@
 "use client";
 import React from "react";
 import Image, { type ImageProps } from "next/image";
+import { getPathWithBasePath } from "@/utils/path";
 
 const PreviewImage = (props: ImageProps) => {
+  const src =
+    typeof props.src === "string" && props.src.startsWith("/")
+      ? getPathWithBasePath(props.src)
+      : props.src;
+
   const handlePreview = () => {
-    if (typeof props?.src === "string") {
-      window.open(props?.src as string);
+    if (typeof src === "string") {
+      window.open(src);
     }
   };
 
-  return <Image {...props} onClick={handlePreview} />;
+  return <Image {...props} src={src} onClick={handlePreview} />;
 };
 
 export default PreviewImage;
